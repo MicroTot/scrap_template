0d488ed87146e873fcce642658329caa536e04b0
MicroTot <32261713+MicroTot@users.noreply.github.com>
2021-05-03 07:56:00 +0000

    fixed bugs

--- core/views.py
+++ core/views.py
@@ -5,17 +5,17 @@ import requests
 # Create your views here.
 def scraped():
     scraps = []
-    url = "https://www.pesapal.com/personal"
+    url = "[url here]"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     # print(soup)
-    rows = soup.find_all(class_= "col-12 col-md-6 col-xl-4 offset-xl-1 order-md-2 aos-init aos-animate")
+    rows = soup.find_all(class_= "")
     # print(rows)
     for r in rows:
         title=r.find('h3').text
         # print(title)
         scrap={
-            
+            'title': title,
         }
         scraps.append(scrap)
     return scraps
@@ -23,6 +23,6 @@ def scraped():
 def load_scraps(request):
     scraps = scraped()
     context = {
-        'scraps': scraps
+        'scraps': scraps,
     }
     return render(request, 'index.html', context)
